8eb59fe24688c1956194d543f4bf066a0240bda8
wahabyounas <wahabyounas1234@gmail.com>
2021-09-09 15:46:37 +0500

    as

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -207,4 +207,25 @@ $('#welcome-carousel').owlCarousel({
 			navbar.classList.remove("sticky");
 		}
 	}
+	/*===========================
+====== Porfolio Filter ======
+=============================*/
+$(document).ready(function () {
+    $(".filter-button").click(function () {
+      var value = $(this).attr('data-filter');
+      if (value == "all") {
+        $('.filter').show('1000');
+      }
+      else {
+        $(".filter").not('.' + value).hide('3000');
+        $('.filter').filter('.' + value).show('3000');
+      }
+  // active
+      $(".filter-button").removeClass('active');
+      var $this = $(this);
+      if (!$this.hasClass('active')) {
+        $this.addClass('active');
+      }
+    });
+  });
 	
